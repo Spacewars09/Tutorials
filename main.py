7a664555df4c2f4caf3fd361a254795c93eab7c4
Spacewars09 <spacewarsy@gmail.com>
2020-12-03 02:19:33 +0000

    More code

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-name = input("Please enter your name:")
-passw = input("Please enter your password:")
+import time
+
+com = input("universe/milky_way/Sun_system/earth/living/human: ")
+if com == "Sudo Login":
+    print(" ")
+    print("Login")
+    print(" ")
+if com == "Sudo Login":
+    name = input("Please enter your name: ")
+    if com == "Sudo Login":
+        passw = input("Please enter your password: ")
+
 if passw == "Password":
-    print("Hello Admin!")
+    print("Login Succes")
+
 else:
     print("Auth failure")
+if passw == "Password":
+    time.sleep(1)
+    print()
+    print("A. Get money")
+    time.sleep(1)
+    print()
+    print("B. Get lots of money")
+    time.sleep(1)
+    print()
+    action1 = input("Your answer: ")
+
+if action1 == "A":
+    print("You have a good life")
+if action1 == "B":
+    ("You are rich and have a very good life")
